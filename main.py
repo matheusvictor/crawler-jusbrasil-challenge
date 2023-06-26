d123d51d07caaf7a7933003c1912b6d3cd6a9f67
Matheus Victor <matheusvictor.salles@gmail.com>
2023-06-25 22:21:59 -0300

    feat: add more generic functions to extract data

--- main.py
+++ main.py
@@ -9,12 +9,27 @@ from utils import save_json_file
 URL: str = 'https://storage.googleapis.com/jus-challenges/challenge-crawler.html'
 
 
-def remove_whitespace_and_line_breaks_from_text(text):
+def remove_duplicated_space_and_line_breaks_from_text(text):
     return re.sub(pattern=r'\s+', repl=' ', string=text.strip())
 
 
-def remove_label_from_text(text, label):
-    return text.replace(label, '')
+def remove_label_from_text(text, regex_pattern):
+    return re.sub(string=text, pattern=regex_pattern, repl='')
+
+
+def extract_date_from_text(text):
+    regex = re.compile(r'\b\d{2}/\d{2}/\d{4}\b')
+    return regex.search(text).group(0)
+
+
+def extract_data_information_from_jurisprudence_raw_data(raw_data, regex_pattern):
+    rows = raw_data.find_all('tr')
+    regex = re.compile(r'{0}'.format(regex_pattern))
+
+    for element in rows:
+        if regex.search(element.text.strip()):
+            return remove_duplicated_space_and_line_breaks_from_text(
+                remove_label_from_text(text=element.text, regex_pattern=regex))
 
 
 def extract_process_number_from_jurisprudence_raw_data(raw_data):
@@ -34,7 +49,8 @@ def extract_summary_from_jurisprudence_raw_data(raw_data):
     for element in rows:
         if regex.search(element.text.strip()):
             summary_div = element.find('div')
-            return remove_whitespace_and_line_breaks_from_text(summary_div.text).replace('Ementa: ', '')
+            summary = remove_label_from_text(text=summary_div.text, regex_pattern=regex)
+            return remove_duplicated_space_and_line_breaks_from_text(summary)
 
 
 def extract_reporter_from_jurisprudence_raw_data(raw_data):
@@ -73,16 +89,24 @@ def main():
 
         jurisprudence_list = list()
         for index, jurisprudence_raw_data in enumerate(section_information_tables):
+            # TODO: Corrigir extração do compo Relator(a)
             jurisprudence = {
                 "id": (index + 1),
                 "numeroProcesso": extract_process_number_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "assunto": extract_subject_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "ementa": extract_summary_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "relator": extract_reporter_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "comarca": extract_county_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "orgaoJulgador": extract_judging_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "dataJulgamento": extract_judgment_date_from_jurisprudence_raw_data(jurisprudence_raw_data),
-                "dataPublicacao": extract_publish_date_from_jurisprudence_raw_data(jurisprudence_raw_data),
+                "assunto": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                regex_pattern='Classe/Assunto:\s+'),
+                "ementa": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                               regex_pattern='Ementa:\s+'),
+                "relator": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                regex_pattern='Relator(a):\s+'),
+                "comarca": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                regex_pattern='Comarca:\s+'),
+                "orgaoJulgador": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                      regex_pattern='Órgão julgador:\s+'),
+                "dataJulgamento": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                       regex_pattern='Data do julgamento:\s+'),
+                "dataPublicacao": extract_data_information_from_jurisprudence_raw_data(raw_data=jurisprudence_raw_data,
+                                                                                       regex_pattern='Data de publicação:\s+'),
             }
 
             jurisprudence_list.append(jurisprudence)
